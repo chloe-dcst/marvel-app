3b29a8c241dbf87fb2cf9461908eff4ad794cf90
Chloé DECOUST <chloe.decoust@etu.univ-poitiers.fr>
2025-10-07 18:00:34 +0200

    chore: 🔧 update devDependencies for Babel, testing libraries, and Jest environment

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,6 @@
 module.exports = {
+  testEnvironment: 'jest-fixed-jsdom', // Use jsdom environment for testing React components
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
+    '^.+\\.jsx?$': 'babel-jest',
   },
 };
